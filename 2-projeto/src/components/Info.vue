38100a088d2a2ed5895350785ee12e5a2ef2e011
helenanns <lenanun3s@gmail.com>
2023-07-03 17:03:12 -0300

    feat(aula-9): Conhecendo as diretivas (v-if, v-show)

--- 2-projeto/src/components/Info.vue
+++ 2-projeto/src/components/Info.vue
@@ -1,20 +1,28 @@
 <template>
-    <p>Estou trabalhando no momento.</p>
-    <p>Utilizo as seguintes tecnologias:</p>
-    <ul>
-        <li>Javascript</li>
-        <li>PHP</li>
-        <li>Python</li>
-    </ul>
+    <div>
+        <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+        <p v-else="esta_trabalhando">Estou em busca de novas oportunidades.</p>
+        <p>Utilizo as seguintes tecnologias:</p>
+        <ul>
+            <li>Javascript</li>
+            <li>PHP</li>
+            <li>Python</li>
+        </ul>
+        <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+    </div>
 </template>
 
 <script>
-export default{
-    name: 'Info',
-    data(){
-        return{
-            nome: 'Helena'
+    export default{
+        name: 'Info',
+        data(){
+            return{
+                email: 'helena.nunes@futurasistemas.com.br',
+                esta_trabalhando: false,
+                mostrar_email: true
+            }
         }
     }
-}
 </script>
+
+
