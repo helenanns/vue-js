7e0ee3120a0e10e61ed5050df4935be3e52011a6
helenanns <lenanun3s@gmail.com>
2023-07-03 16:30:01 -0300

    feat(aula-8): Hierarquia de componentes

---
+++ 2-projeto/src/components/Info.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>Estou trabalhando no momento.</p>
+    <p>Utilizo as seguintes tecnologias:</p>
+    <ul>
+        <li>Javascript</li>
+        <li>PHP</li>
+        <li>Python</li>
+    </ul>
+</template>
+
+<script>
+export default{
+    name: 'Info',
+    data(){
+        return{
+            nome: 'Helena'
+        }
+    }
+}
+</script>
---
+++ 2-projeto/src/components/Pessoa.vue
@@ -0,0 +1,22 @@
+<template>
+    <h2>Descrição da pessoa: {{ nome }}</h2>
+    <Info /> 
+    <Form />
+</template>
+
+<script>
+import Info from './Info.vue'
+import Form from './Form.vue'
+export default{
+    name: 'Pessoa',
+    components:{
+        Info,
+        Form
+    },
+    data(){
+        return{
+            nome: 'Helena'
+        }
+    }
+}
+</script>
