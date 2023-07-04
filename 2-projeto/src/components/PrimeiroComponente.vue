d904e88916a6be179d9b0609298a884b5b3735ef
helenanns <lenanun3s@gmail.com>
2023-07-04 11:31:57 -0300

    feat(aula-16): Reutilização de componentes

--- 2-projeto/src/components/PrimeiroComponente.vue
+++ 2-projeto/src/components/PrimeiroComponente.vue
@@ -1,13 +1,17 @@
 <template>
     <h1>Oi!</h1>
     <MultiplosEventos /> 
+    <Reutilizacao /> 
+    <Reutilizacao /> 
 </template>
 
 
 <script>
     import MultiplosEventos from './MultiplosEventos.vue';
+    import Reutilizacao from './Reutilizacao.vue';
+
     export default{
         name: 'PrimeiroComponente',
-        components: { MultiplosEventos }
+        components: { MultiplosEventos, Reutilizacao }
     }
 </script>
