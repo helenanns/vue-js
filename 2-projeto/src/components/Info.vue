1ef667cf82729e363242d4612040c20ff40a963e
helenanns <lenanun3s@gmail.com>
2023-07-04 08:52:34 -0300

    feat(aula-10): Atributos dinâmicos (v-bind)

--- 2-projeto/src/components/Info.vue
+++ 2-projeto/src/components/Info.vue
@@ -9,17 +9,24 @@
             <li>Python</li>
         </ul>
         <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
+        <p>Para acessar meu portfólio <a v-bind:href="meu_link" target="_blank">clique aqui</a></p>
+        <Picture />
     </div>
 </template>
 
 <script>
+    import Picture from './Picture.vue';
     export default{
         name: 'Info',
+        components: {
+            Picture
+        },
         data(){
             return{
                 email: 'helena.nunes@futurasistemas.com.br',
                 esta_trabalhando: false,
-                mostrar_email: true
+                mostrar_email: true,
+                meu_link: 'https://google.com'
             }
         }
     }
---
+++ 2-projeto/src/components/Picture.vue
@@ -0,0 +1,15 @@
+<template>
+    <img :src="avatar" alt="" height="50" width="50">
+</template>
+
+
+<script>
+    export default{
+        name: 'Picture',
+        data(){
+            return{
+                avatar: '/img/avatar.png'
+            }
+        }
+    }
+</script>
